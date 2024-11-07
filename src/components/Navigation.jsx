6ca2e55df4395f7ade1ba8279dbdbaded50cff1a
YuvCarl <carldrake969@gmail.com>
2024-11-07 15:27:08 +0100

    updated the nav overlay for mobile

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -28,6 +28,18 @@ export default function Navigation() {
             <img src={chopasapLogo} alt="logo-mobile" className='logo-mobile'/>
             <i class="bi bi-list"></i>
           </div>
+
+          <div className="nav-overlay">
+          <i class="bi bi-x-lg relative"></i>
+          <img src={logo} alt="logo" className='logo'/>
+            <div className="nav-overlay-content">
+              <Link to="/home" className='nav-ell-overlay'>Home</Link>
+              <Link to="/services" className='nav-ell-overlay'>Services</Link>
+              <Link to="/faqs" className='nav-ell-overlay'>FAQs</Link>
+              <Link to="/blog" className='nav-ell-overlay'>Blog</Link>
+              <Link to="/contact" className='nav-ell-overlay'>Contact</Link>
+            </div>
+          </div>
       
     </div>
 
