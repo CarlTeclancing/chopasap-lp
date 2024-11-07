d181f95ebd6081ac57a3f82d3b5fc8164565acc3
YuvCarl <carldrake969@gmail.com>
2024-11-07 15:38:50 +0100

    added css to the mobile tugle and configured to work

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import  logo  from '../assets/logo.svg'
 import {  Link } from 'react-router-dom';
 import Relay from '../pages/Services';
@@ -7,7 +8,8 @@ import chopasapLogo from '../assets/chopasapLogo.png';
 
 
 export default function Navigation() {
-  
+
+  const [action, setAction] = useState('open');
   return (
     <div className='nav-container'>
       <div className='navigation'>
@@ -26,18 +28,19 @@ export default function Navigation() {
       </div>    
           <div className='mobile-nav'>
             <img src={chopasapLogo} alt="logo-mobile" className='logo-mobile'/>
-            <i class="bi bi-list"></i>
+            <i class="bi bi-list" onClick={()=> setAction("open")}></i>
           </div>
 
-          <div className="nav-overlay">
-          <i class="bi bi-x-lg relative"></i>
+          <div className={action==="close"?"none":"nav-overlay"}>
+          
+          <i class="bi bi-x-lg relative" onClick={()=> setAction("close")}></i>
           <img src={logo} alt="logo" className='logo'/>
             <div className="nav-overlay-content">
-              <Link to="/home" className='nav-ell-overlay'>Home</Link>
-              <Link to="/services" className='nav-ell-overlay'>Services</Link>
-              <Link to="/faqs" className='nav-ell-overlay'>FAQs</Link>
-              <Link to="/blog" className='nav-ell-overlay'>Blog</Link>
-              <Link to="/contact" className='nav-ell-overlay'>Contact</Link>
+              <Link to="/home" className='nav-ell-overlay'  onClick={()=> setAction("close")}>Home</Link>
+              <Link to="/services" className='nav-ell-overlay'  onClick={()=> setAction("close")}>Services</Link>
+              <Link to="/faqs" className='nav-ell-overlay'  onClick={()=> setAction("close")}>FAQs</Link>
+              <Link to="/blog" className='nav-ell-overlay'  onClick={()=> setAction("close")}>Blog</Link>
+              <Link to="/contact" className='nav-ell-overlay'  onClick={()=> setAction("close")}>Contact</Link>
             </div>
           </div>
       
